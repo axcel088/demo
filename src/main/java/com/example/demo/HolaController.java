3d8695f91d6f2a4c7e9a1c2568f9e0707c3c08f2
Abel <abelmaxeo088@gmail.com>
2026-05-06 16:06:55 -0500

    deploy

---
+++ src/main/java/com/example/demo/HolaController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+ @GetMapping("/")
+ public String saludo() {
+ return "Hola, mi aplicación ya está en Internet ";
+ }
+}
